7228855de78324477f09bfa478f6dd65e21ebb9d
Shruthi Janardhan <sjanardhan@corp.broadsoft.com>
2016-12-05 16:09:34 +0530

    learnt commit

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 
-// describe("true",function(){
-// 	it("Should be true",function(){
-// 		expect(true).toBeTruthy();
-// 	})
-// });
+ describe("true",function(){
+ 	it("Should be true",function(){
+ 		expect(true).toBeTruthy();
+ 	})
+});
